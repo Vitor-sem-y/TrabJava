6fe3fe5e5e30230389a3e55f21de8531df387d3f
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-03-29 11:39:45 -0300

    override

--- src/model/RegistroMedicoTableModel.java
+++ src/model/RegistroMedicoTableModel.java
@@ -3,15 +3,36 @@ package model;
 
 import java.util.List;
 import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
 import programamedico2.ProgramaMedico2;
 
 /**
  *
  * @author Pedro Henrique
  */
-public class RegistroMedicoTableModel {
+public class RegistroMedicoTableModel extends AbstractTableModel{
     
     private List<ProgramaMedico2> list = new ArrayList<>();
-    private final String[] columnNames = {}; // TchuTchu cão
-c    
+    private final String[] columnNames = {"No. RM", "Nome", 
+        "Endereço", "Classe", "Sala"}; // TchuTchu cão, JS é o grandão
+
+    public void setList(List<ProgramaMedico2> list){
+        this.list = list;
+        fireTableDataChanged();
+    }
+    
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }

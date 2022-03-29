1cad84954351253187c90f3dccb00f2040e32ec5
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-03-29 11:25:42 -0300

    MOdificação dos Arquivos

---
+++ src/model/RegistroMedicoTableModel.java
@@ -0,0 +1,17 @@
+
+package model;
+
+import java.util.List;
+import java.util.ArrayList;
+import programamedico2.ProgramaMedico2;
+
+/**
+ *
+ * @author Pedro Henrique
+ */
+public class RegistroMedicoTableModel {
+    
+    private List<ProgramaMedico2> list = new ArrayList<>();
+    private final String[] columnNames = {}; // TchuTchu cão
+c    
+}
--- src/programamedico2/ProgramaMedico2.java
+++ src/programamedico2/ProgramaMedico2.java
@@ -11,6 +11,52 @@ package programamedico2;
  */
 public class ProgramaMedico2 {
 
+    private String rm;
+    private String paciente;
+    private String endereco;
+    private String classe;
+    private String sala;
+
+    public String getRm() {
+        return rm;
+    }
+
+    public void setRm(String rm) {
+        this.rm = rm;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+    
     /**
      * @param args the command line arguments
      */
